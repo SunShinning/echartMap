ae7052fc57313395950dcaf84aca2b4227ae0444
caoshengtang <caoshengtang>
2017-08-04 18:57:56 +0800

    123

--- js/timeAv.js
+++ js/timeAv.js
@@ -1,8 +1,4 @@
-/**
- * Created by admin on 2017/8/4.
- */
 var myChart = echarts.init(document.getElementById('main'));
-
 var geoCoordMap = {
     '上海': [121.4648,31.2891],
     '东莞': [113.8953,22.901],
@@ -121,42 +117,17 @@ var geoCoordMap = {
 };
 
 var BJData = [
-    [{name:'北京'}, {name:'上海',value:95}],
-    [{name:'北京'}, {name:'广州',value:90}],
-    [{name:'北京'}, {name:'大连',value:80}],
-    [{name:'北京'}, {name:'南宁',value:70}],
-    [{name:'北京'}, {name:'南昌',value:60}],
-    [{name:'北京'}, {name:'拉萨',value:50}],
-    [{name:'北京'}, {name:'长春',value:40}],
-    [{name:'北京'}, {name:'包头',value:30}],
-    [{name:'北京'}, {name:'重庆',value:20}],
-    [{name:'北京'}, {name:'常州',value:10}]
+    [{name:'成都'}, {name:'宁波',value:95}],
+    [{name:'成都'}, {name:'广州',value:95}],
 ];
 
 var SHData = [
-    [{name:'上海'},{name:'包头',value:95}],
-    [{name:'上海'},{name:'昆明',value:90}],
-    [{name:'上海'},{name:'广州',value:80}],
-    [{name:'上海'},{name:'郑州',value:70}],
-    [{name:'上海'},{name:'长春',value:60}],
-    [{name:'上海'},{name:'重庆',value:50}],
-    [{name:'上海'},{name:'长沙',value:40}],
-    [{name:'上海'},{name:'北京',value:30}],
-    [{name:'上海'},{name:'丹东',value:20}],
-    [{name:'上海'},{name:'大连',value:10}]
+    [{name:'成都'},{name:'苏州',value:95}],
+    [{name:'成都'},{name:'杭州',value:95}],
 ];
 
 var GZData = [
-    [{name:'广州'},{name:'福州',value:95}],
-    [{name:'广州'},{name:'太原',value:90}],
-    [{name:'广州'},{name:'长春',value:80}],
-    [{name:'广州'},{name:'重庆',value:70}],
-    [{name:'广州'},{name:'西安',value:60}],
-    [{name:'广州'},{name:'成都',value:50}],
-    [{name:'广州'},{name:'常州',value:40}],
-    [{name:'广州'},{name:'北京',value:30}],
-    [{name:'广州'},{name:'北海',value:20}],
-    [{name:'广州'},{name:'海口',value:10}]
+    [{name:'成都'},{name:'武汉',value:95}],
 ];
 
 var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
@@ -178,12 +149,13 @@ var convertData = function (data) {
     return res;
 };
 
-var color = ['red', '#ffa022', '#46bee9'];
+var color = ['#a6c84c', '#ffa022', '#46bee9'];
 var series = [];
-[['北京', BJData], ['上海', SHData], ['广州', GZData]].forEach(function (item, i) {
+[['邮政领先', BJData], ['二者持平', SHData], ['邮政落后', GZData]].forEach(function (item, i) {
     series.push({
-            name: item[0] + ' Top10',
+            name: item[0],
             type: 'lines',
+            coordinateSystem:'geo',
             zlevel: 1,
             effect: {
                 show: true,
@@ -192,9 +164,16 @@ var series = [];
                 color: '#fff',
                 symbolSize: 3
             },
+            label: {
+                normal: {
+                    show: true,
+                    position: 'right',
+                    formatter: '{b}'
+                }
+            },
             lineStyle: {
                 normal: {
-                    color: '0',
+                    color: color[i],
                     width: 0,
                     curveness: 0.2
                 }
@@ -202,8 +181,9 @@ var series = [];
             data: convertData(item[1])
         },
         {
-            name: item[0] + ' Top10',
+            name: item[0],
             type: 'lines',
+            coordinateSystem:'geo',
             zlevel: 2,
             symbol: ['none', 'arrow'],
             symbolSize: 10,
@@ -225,7 +205,7 @@ var series = [];
             data: convertData(item[1])
         },
         {
-            name: item[0] + ' Top10',
+            name: item[0],
             type: 'effectScatter',
             coordinateSystem: 'geo',
             zlevel: 2,
@@ -273,17 +253,17 @@ option = {
         orient: 'vertical',
         top: 'bottom',
         left: 'right',
-        data:['北京 Top10', '上海 Top10', '广州 Top10'],
+        data:['邮政领先', '二者持平', '邮政落后'],
         textStyle: {
             color: '#fff'
         },
-        selectedMode: 'single'
+        //selectedMode: 'single'
     },
     geo: {
         map: 'china',
         label: {
-            emphasis: {
-                show: false
+            normal: {
+                show: true
             }
         },
         roam: true,
